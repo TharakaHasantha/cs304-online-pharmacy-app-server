bf42728c4e78ef8894f9ada18af75f7bd6546a42
TharakaHasantha <tharakahasantha110@gmail.com>
2021-07-29 18:44:13 +0530

    check one by one routers

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,9 +4,9 @@ const cartControllers = require("../controllers/cartControllers");
 var router=express.Router();
 
 router.post("/add",cartControllers.addToCart);
-router.post("/getItemsCount",cartControllers.getItemsCount)
-router.post("/getItems",cartControllers.getItems)
-router.post("/removeItem",cartControllers.removeItem)
-router.post("/updateItem",cartControllers.updateItem)
+// router.post("/getItemsCount",cartControllers.getItemsCount)
+// router.post("/getItems",cartControllers.getItems)
+// router.post("/removeItem",cartControllers.removeItem)
+// router.post("/updateItem",cartControllers.updateItem)
 
 module.exports=router;
